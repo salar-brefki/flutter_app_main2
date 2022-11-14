b6d574c95227546b75949094718f60cfe5c448e7
Salar Brefki <93735640+salar-brefki@users.noreply.github.com>
2022-11-14 19:58:37 +0300

    update UI

--- lib/login/select_lang_screen.dart
+++ lib/login/select_lang_screen.dart
@@ -29,177 +29,181 @@ class _SelectLangState extends State<SelectLang> {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            flex: 3,
-            child: Stack(
-              children: [
-                SizedBox(
-                  width: width,
-                  child: Image.asset(
-                    'assets/login_logo/select_lang.png',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Container(
-                  color: Colors.black.withOpacity(0.1),
-                ),
-                Center(
-                    child: Text(
-                  translation(context).busBag,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: width * 0.11,
-                    fontWeight: FontWeight.w900,
-                  ),
-                )),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              width: width,
-              color: Colors.grey.withOpacity(0.3),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+      body: SizedBox(
+        width: width,
+        height: height,
+        child: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Stack(
                 children: [
-                  Text(
-                    'Choose a language',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: width * 0.07,
-                      fontWeight: FontWeight.bold,
+                  SizedBox(
+                    width: width,
+                    child: Image.asset(
+                      'assets/login_logo/select_lang.png',
+                      fit: BoxFit.cover,
                     ),
                   ),
-                  Text(
-                    'الرجاء اختيار اللغة',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: width * 0.07,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 15),
                   Container(
-                    width: width - 50,
-                    height: 50,
-                    decoration: const BoxDecoration(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(8),
+                    color: Colors.black.withOpacity(0.1),
+                  ),
+                  Center(
+                      child: Text(
+                    translation(context).busBag,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: width * 0.11,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  )),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                width: width,
+                color: Colors.grey.withOpacity(0.3),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Choose a language',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: width * 0.07,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: GestureDetector(
-                            onTap: () async {
-                              Locale locale = await setLocale("ar");
-                              MyApp.setLocale(context, locale);
-                              setState(() {
-                                langSelect = "ar";
-                              });
-                            },
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: langSelect == 'en'
-                                    ? const Color.fromARGB(255, 255, 255, 255)
-                                    : const Color.fromARGB(255, 33, 105, 181),
-                                borderRadius: const BorderRadius.all(
-                                  Radius.circular(8),
-                                ),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  'العربية',
-                                  style: TextStyle(
-                                    color: langSelect == 'en'
-                                        ? Colors.black
-                                        : Colors.white,
-                                    fontSize: width * 0.040,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: GestureDetector(
-                            onTap: () async {
-                              Locale locale = await setLocale("en");
-                              MyApp.setLocale(context, locale);
-                              setState(() {
-                                langSelect = "en";
-                              });
-                            },
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: langSelect == 'ar'
-                                    ? const Color.fromARGB(255, 255, 255, 255)
-                                    : const Color.fromARGB(255, 33, 105, 181),
-                                borderRadius: const BorderRadius.all(
-                                  Radius.circular(8),
-                                ),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  'English',
-                                  style: TextStyle(
-                                    color: langSelect == 'ar'
-                                        ? Colors.black
-                                        : Colors.white,
-                                    fontSize: width * 0.040,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        )
-                      ],
+                    Text(
+                      'الرجاء اختيار اللغة',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: width * 0.07,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 35),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const IntroScreen(),
-                        ),
-                      );
-                    },
-                    child: Container(
+                    const SizedBox(height: 15),
+                    Container(
                       width: width - 50,
                       height: 50,
                       decoration: const BoxDecoration(
-                        color: Color.fromARGB(255, 33, 105, 181),
+                        color: Color.fromARGB(255, 255, 255, 255),
                         borderRadius: BorderRadius.all(
                           Radius.circular(8),
                         ),
                       ),
-                      child: Center(
-                        child: Text(
-                          translation(context).next,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: width * 0.040,
-                            fontWeight: FontWeight.bold,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: GestureDetector(
+                              onTap: () async {
+                                Locale locale = await setLocale("ar");
+                                MyApp.setLocale(context, locale);
+                                setState(() {
+                                  langSelect = "ar";
+                                });
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: langSelect == 'en'
+                                      ? const Color.fromARGB(255, 255, 255, 255)
+                                      : const Color.fromARGB(255, 33, 105, 181),
+                                  borderRadius: const BorderRadius.all(
+                                    Radius.circular(8),
+                                  ),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    'العربية',
+                                    style: TextStyle(
+                                      color: langSelect == 'en'
+                                          ? Colors.black
+                                          : Colors.white,
+                                      fontSize: width * 0.040,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: GestureDetector(
+                              onTap: () async {
+                                Locale locale = await setLocale("en");
+                                MyApp.setLocale(context, locale);
+                                setState(() {
+                                  langSelect = "en";
+                                });
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: langSelect == 'ar'
+                                      ? const Color.fromARGB(255, 255, 255, 255)
+                                      : const Color.fromARGB(255, 33, 105, 181),
+                                  borderRadius: const BorderRadius.all(
+                                    Radius.circular(8),
+                                  ),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    'English',
+                                    style: TextStyle(
+                                      color: langSelect == 'ar'
+                                          ? Colors.black
+                                          : Colors.white,
+                                      fontSize: width * 0.040,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 35),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const IntroScreen(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        width: width - 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          color: Color.fromARGB(255, 33, 105, 181),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(8),
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            translation(context).next,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: width * 0.040,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
